47257fd5c9af1642e839929feb4aa84a6b68de6b
khaihoan <ngonguyenkhaihoan@gmail.com>
2021-07-27 14:47:21 +0700

    bt

---
+++ Adam and Eva/ja2.js
@@ -0,0 +1,26 @@
+class Humman {
+    name;
+    gender;
+    weight;
+    constructor(name, gender, weight) {
+        this.name = String;
+        this.gender = boolean;
+        this.weight = weight;
+    }
+    getName(){
+        return this.name
+    }
+    isMale(boolean){
+        return "";
+    }
+    getGender(boolean){
+        return this.gender;
+    }
+    eat(Apple){
+        return ""
+    }
+    getWeight(){
+        return this.weight;
+    }
+}
+let humman = Humman(Tấn,nam, 50)
---
+++ Adam and Eva/js.js
@@ -0,0 +1,23 @@
+class Apple{
+    weight;
+    mass;
+    constructor(weight, mass) {
+        this.weight = weight;
+    }
+    getWeight(){
+        return this.weight;
+    }
+    getMass(){
+        return this.getMass(10);
+    }
+    decrease(){
+        return ""
+    }
+    isEmpty(){
+        return ""
+    }
+    Apple(){
+        return""
+    }
+}
+let apple = Apple(10,10)
